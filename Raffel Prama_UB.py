894245a3cd58debea4beb6a929bb10e56059747e
the008 <87288406+prm008@users.noreply.github.com>
2021-10-08 21:12:58 +0700

    Add files via upload

---
+++ Raffel Prama_UB.py
@@ -0,0 +1,13 @@
+"""
+tugas Identitas GDSC ITB
+menuliskan ke layar nama, jurusan, alasan
+"""
+
+nama = "Raffel Prama"
+jurusan = "Elektro UB"
+Alasan = "Karena saya melihat peluang pengembangan kemampuan saya terhadap pemograman yang cukup efektif jika saya ikut kegiatn ini untuk menambah relasi mau teman maupun mentor dan secara spesifik aya juga karena cukup tertarik dengan ML\ndan karena di GDSC ini memiliki kurikulumnya maka saya makin semangat"
+
+
+print("nama : " + nama)
+print("jurusan : " +jurusan)
+print("alasan ingin masuk GDSC : " + Alasan)
